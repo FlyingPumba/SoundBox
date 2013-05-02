a2756b82888bb8d704e256bee72f9e3fd5ee4c84
Ivan Arcuschin <iarcuschin@gmail.com>
2013-05-02 02:04:12 +0200

    Now it does play songs (only from the SongsActivity)

--- src/com/arcusapp/arcusmusic/PlayActivity.java
+++ src/com/arcusapp/arcusmusic/PlayActivity.java
@@ -4,14 +4,22 @@ import java.io.File;
 import java.util.List;
 
 import android.app.Activity;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class PlayActivity extends Activity {
+public class PlayActivity extends Activity implements OnClickListener{
 
 	TextView txtTitle, txtFile, txtArtist, txtAlbum;
+	Button btnPlayPause, btnPrev, btnNext, btnLogo5, btnSwichRandom, btnSwichRepeat, btnList;
+	MediaPlayer mediaPlayer;
+	
 	private SongsHandler sh;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +32,9 @@ public class PlayActivity extends Activity {
 		txtFile = (TextView)findViewById(R.id.txtActualSongFile);
 		txtArtist = (TextView)findViewById(R.id.txtActualSongArtist);
 		txtAlbum = (TextView)findViewById(R.id.txtActualSongAlbum);
+		
+		btnPlayPause = (Button)findViewById(R.id.btnPlayPause);
+		btnPlayPause.setOnClickListener(this);
 		
 		 //Recuperamos la información pasada en el intent
         Bundle bundle = this.getIntent().getExtras();
@@ -38,6 +49,10 @@ public class PlayActivity extends Activity {
         txtFile.setText("Filename: "+file.getName());
         txtArtist.setText("Artist: "+infoSong.get(2));
         txtAlbum.setText("Album: "+infoSong.get(3));
+        
+        Uri uri = Uri.fromFile(file);
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), uri);
+        mediaPlayer.start();
 
 	}
 
@@ -48,4 +63,23 @@ public class PlayActivity extends Activity {
 		return true;
 	}
 
+	@Override
+	public void onClick(View v) {
+		if(v.getId() == R.id.btnPlayPause){
+			if(mediaPlayer.isPlaying() == false)
+				mediaPlayer.start();
+			else
+				mediaPlayer.pause();
+		}
+	}
+	
+	@Override
+    protected void onDestroy() {
+		super.onDestroy();
+        if(mediaPlayer != null) {
+            mediaPlayer.stop();
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+    }
 }
