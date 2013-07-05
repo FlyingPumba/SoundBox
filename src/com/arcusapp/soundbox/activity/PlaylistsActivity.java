8c8f89985c9b9fee3c26de303045a25260d6e21f
Ivan <iarcuschin@gmail.com>
2013-07-05 14:14:07 -0300

    Cleaned the PlaylistsActivity

--- src/com/arcusapp/soundbox/activity/PlaylistsActivity.java
+++ src/com/arcusapp/soundbox/activity/PlaylistsActivity.java
@@ -11,21 +11,19 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemLongClickListener;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ListView;
 
 import com.arcusapp.soundbox.R;
+import com.arcusapp.soundbox.SoundBoxApplication;
 import com.arcusapp.soundbox.data.MediaProvider;
 import com.arcusapp.soundbox.model.BundleExtra;
 import com.arcusapp.soundbox.model.PlaylistEntry;
 import com.arcusapp.soundbox.util.MediaEntryHelper;
 
-public class PlaylistsActivity extends ListActivity implements
-		View.OnClickListener {
+public class PlaylistsActivity extends ListActivity implements View.OnClickListener {
 
-	private Button btnLogo6;
 	private ListView listView;
-	private MediaProvider media;
+	private MediaProvider mediaProvider;
 	private MediaEntryHelper<PlaylistEntry> mediaEntryHelper;
 	private Intent playActivityIntent;
 	private List<PlaylistEntry> playLists;
@@ -35,12 +33,8 @@ public class PlaylistsActivity extends ListActivity implements
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_play_lists);
 
-		media = new MediaProvider();
+		mediaProvider = new MediaProvider();
 		mediaEntryHelper = new MediaEntryHelper<PlaylistEntry>();
-
-		// TODO: set the onClick method on the layout xml
-		btnLogo6 = (Button) findViewById(R.id.btnLogo6);
-		btnLogo6.setOnClickListener(this);
 
 		listView = (ListView) findViewById(android.R.id.list);
 
@@ -48,11 +42,8 @@ public class PlaylistsActivity extends ListActivity implements
 		listView.setOnItemLongClickListener(new OnItemLongClickListener() {
 
 			@Override
-			public boolean onItemLongClick(AdapterView<?> arg0, View arg1,
-					int pos, long id) {
-				// we directly play all the playlist
+			public boolean onItemLongClick(AdapterView<?> arg0, View arg1, int pos, long id) {
 
-				// Creamos el Intent
 				playActivityIntent = new Intent();
 				playActivityIntent.setAction("com.arcusapp.soundbox.PLAY_ACTIVITY");
 
@@ -62,7 +53,7 @@ public class PlaylistsActivity extends ListActivity implements
 				b.putString(BundleExtra.CURRENT_ID, BundleExtra.DefaultValues.DEFAULT_ID);
 
 				String playlistID = playLists.get(pos).getID();
-				b.putStringArrayList(BundleExtra.SONGS_ID_LIST, new ArrayList<String>(media.getSongsFromPlaylist(playlistID)));
+				b.putStringArrayList(BundleExtra.SONGS_ID_LIST, new ArrayList<String>(mediaProvider.getSongsFromPlaylist(playlistID)));
 
 				playActivityIntent.putExtras(b);
 				startActivity(playActivityIntent);
@@ -71,15 +62,13 @@ public class PlaylistsActivity extends ListActivity implements
 			}
 		});
 
-		playLists = media.getAllPlayLists();
+		playLists = mediaProvider.getAllPlayLists();
 
 		setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mediaEntryHelper.getValues(playLists)));
-
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.play_lists, menu);
 		return true;
 	}
@@ -88,24 +77,15 @@ public class PlaylistsActivity extends ListActivity implements
 	protected void onListItemClick(ListView l, View v, int position, long id) {
 		super.onListItemClick(l, v, position, id);
 
-		// mostramos las canciones de esa lista de reproduccion llamando al
-		// SongsListActivity
-
-		// Creamos el Intent
+		// show the songs from that specific playlists on the SongsListActivity
 		playActivityIntent = new Intent();
-		playActivityIntent.setAction("com.arcusapp.soundbox.SONGSLIST_ACTIVITY");
+		playActivityIntent.setAction(SoundBoxApplication.ACTION_SONGLIST_ACTIVITY);
 
-		// Creamos la informacion a pasar entre actividades
 		Bundle b = new Bundle();
-
 		String playlistID = playLists.get(position).getID();
-		b.putStringArrayList("songs",
-				new ArrayList<String>(media.getSongsFromPlaylist(playlistID)));
-
-		// Anadimos la informacion al intent
+		b.putStringArrayList(BundleExtra.SONGS_ID_LIST, new ArrayList<String>(mediaProvider.getSongsFromPlaylist(playlistID)));
 		playActivityIntent.putExtras(b);
 
-		// Iniciamos la nueva actividad
 		startActivity(playActivityIntent);
 	}
 
